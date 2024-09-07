28883e1efa02f3a85b5b20ceb4bdbd95322aaf70
evariste1963 <evariste@hotmail.co.uk>
2024-09-07 08:36:01 +0100

    1:45:00 basically done?

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"html/template"
 	"io"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -16,21 +18,26 @@ func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Co
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
-func NewTemplates() *Templates {
+func newTemplate() *Templates {
 	return &Templates{
 		templates: template.Must(template.ParseGlob("views/*.html")),
 	}
 }
 
+var id = 0
+
 type Contact struct {
 	Name  string
 	Email string
+	Id    int
 }
 
 func newContact(name, email string) Contact {
+	id++
 	return Contact{
 		Name:  name,
 		Email: email,
+		Id:    id,
 	}
 }
 
@@ -38,6 +45,15 @@ type Contacts = []Contact
 
 type Data struct {
 	Contacts Contacts
+}
+
+func (d *Data) indexOf(id int) int {
+	for i, contact := range d.Contacts {
+		if contact.Id == id {
+			return i
+		}
+	}
+	return -1
 }
 
 func (d Data) hasEmail(email string) bool {
@@ -52,7 +68,7 @@ func (d Data) hasEmail(email string) bool {
 func newData() Data {
 	return Data{
 		Contacts: []Contact{
-			newContact("John Doe", "jd@gmail.com"),
+			newContact("John Doe", "qwer"),
 			newContact("Clara Doe", "cd@gmail.com"),
 		},
 	}
@@ -86,7 +102,10 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	page := newPage()
-	e.Renderer = NewTemplates()
+	e.Renderer = newTemplate()
+
+	e.Static("/images", "images")
+	e.Static("/css", "css")
 
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(200, "index", page)
@@ -110,6 +129,22 @@ func main() {
 
 		c.Render(200, "form", newFormData())
 		return c.Render(200, "oob-contact", contact)
+	})
+
+	e.DELETE("/contacts/:id", func(c echo.Context) error {
+		time.Sleep(3 * time.Second)
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			return c.String(400, "Invalid Id")
+		}
+		index := page.Data.indexOf(id)
+		if index == -1 {
+			return c.String(404, "Contact not found")
+		}
+
+		page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+		return c.NoContent(200)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
